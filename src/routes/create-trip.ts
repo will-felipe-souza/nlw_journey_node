6e07ea054a5b3a72b04ffe824608a942f5cfffb4
will-felipe-souza <will.felipe.souza@gmail.com>
2024-07-09 17:16:26 -0300

    add validation for trip date

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+import dayjs from "dayjs";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
@@ -17,6 +18,14 @@ export async function createTrip(app: FastifyInstance) {
     },
     async (req, res) => {
       const { destination, starts_at, ends_at } = req.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Invalid trip start date.");
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Invalid trip end date.");
+      }
 
       const trip = await prisma.trip.create({
         data: {
